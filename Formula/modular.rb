41a77b39c98519af1a9f94cc62c7b2229d56f5fa
Zac Bowling <zac@modular.com>
2023-09-11 16:30:44 -0700

    Create modularml/modular tap

---
+++ Formula/modular.rb
@@ -0,0 +1,21 @@
+class Modular < Formula
+  desc "Modular developer CLI tool"
+  homepage "https://www.modular.com"
+  version "0.0.1"
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://dl.modular.com/public/installer/raw/names/Modular%20CLI%20for%20MacOS%20ARM64/versions/0.0.1/modular-0.0.1-macos-arm64.tar.gz"
+    sha256 "90f7e0a5145eff2f32afa28bf4856e30df373b39380ef3dcc1a8fa62e9b719da"
+  end
+
+  depends_on arch: :arm64
+  depends_on :macos
+
+  def install
+    prefix.install Dir["*"]
+  end
+
+  test do
+    system "#{bin}/modular --version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Modular CLI Homebrew Tap
+
+## How do I install these formulae?
+
+`brew install modularml/modular/modular`
+
+Or `brew tap modularml/modular` and then `brew install modular`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
