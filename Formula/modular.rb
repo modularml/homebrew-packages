1eec8d658c696bb8821d8bc5449c83b1a3f36a47
Aman LaChapelle <aman@modular.com>
2023-09-27 16:48:55 -0700

    Include a way to install the test packages

--- Formula/modular.rb
+++ Formula/modular.rb
@@ -3,14 +3,19 @@ class Modular < Formula
   homepage "https://www.modular.com"
   version "0.0.1"
 
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://dl.modular.com/public/installer/raw/names/modular-mac-arm64/versions/0.0.1/modular-0.0.1-macos-arm64.tar.gz"
-    sha256 "80abde7c4742b680a155cb7c3b80abcf859d556a55bc95f01ac6c754566a19e7"
-  end
-
   depends_on arch: :arm64
   depends_on :macos
   depends_on "python@3.11"
+
+  option "with-testrepo"
+
+  if build.with? "testrepo"
+    url "https://dl.modular.com/ksFEA5DYuqJJ84kb/installer-test/raw/names/modular-mac-arm64/versions/0.0.1/modular-0.0.1-macos-arm64.tar.gz"
+    sha256 "320ca46f7478163e46e03cdcf72b44b53a55ad25d271d4c688af89dff34a37cb"
+  else
+    url "https://dl.modular.com/public/installer/raw/names/modular-mac-arm64/versions/0.0.1/modular-0.0.1-macos-arm64.tar.gz"
+    sha256 "80abde7c4742b680a155cb7c3b80abcf859d556a55bc95f01ac6c754566a19e7"
+  end
 
   def install
     prefix.install Dir["*"]
