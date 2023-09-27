1507dd6dece6897a69cad58df880ee423d243574
Aman LaChapelle <aman@modular.com>
2023-09-27 16:53:49 -0700

    Include a way to install the test packages

--- Formula/modular.rb
+++ Formula/modular.rb
@@ -7,14 +7,12 @@ class Modular < Formula
   depends_on :macos
   depends_on "python@3.11"
 
-  option "with-testrepo"
-
-  if build.with? "testrepo"
-    url "https://dl.modular.com/ksFEA5DYuqJJ84kb/installer-test/raw/names/modular-mac-arm64/versions/0.0.1/modular-0.0.1-macos-arm64.tar.gz"
-    sha256 "320ca46f7478163e46e03cdcf72b44b53a55ad25d271d4c688af89dff34a37cb"
-  else
+  if "#{ENV["HOMEBREW_MODULAR_URL_SLUG"]}".empty?
     url "https://dl.modular.com/public/installer/raw/names/modular-mac-arm64/versions/0.0.1/modular-0.0.1-macos-arm64.tar.gz"
     sha256 "80abde7c4742b680a155cb7c3b80abcf859d556a55bc95f01ac6c754566a19e7"
+  else
+    url "https://dl.modular.com/#{ENV["HOMEBREW_MODULAR_URL_SLUG"]}/raw/names/modular-mac-arm64/versions/0.0.1/modular-0.0.1-macos-arm64.tar.gz"
+    sha256 "#{ENV["HOMEBREW_MODULAR_SHA256"]}"
   end
 
   def install
