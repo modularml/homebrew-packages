c77bbd47a3a8b47108b27898bc543b024e2d6d7f
Aman LaChapelle <aman@modular.com>
2023-09-28 09:11:16 -0700

    Update to v0.2.0

--- Formula/modular.rb
+++ Formula/modular.rb
@@ -1,7 +1,7 @@
 class Modular < Formula
   desc "Modular developer CLI tool"
   homepage "https://www.modular.com"
-  version "0.0.1"
+  version "0.2.0"
 
   depends_on arch: :arm64
   depends_on :macos
@@ -9,7 +9,7 @@ class Modular < Formula
 
   if "#{ENV["HOMEBREW_MODULAR_URL_SLUG"]}".empty?
     url "https://dl.modular.com/public/installer/raw/names/modular-mac-arm64/versions/0.0.1/modular-0.0.1-macos-arm64.tar.gz"
-    sha256 "80abde7c4742b680a155cb7c3b80abcf859d556a55bc95f01ac6c754566a19e7"
+    sha256 "726358aaefbbf9ae9ea820e36822a19080c7fac1b32dcdc2731a28822ca52a5d"
   else
     url "https://dl.modular.com/#{ENV["HOMEBREW_MODULAR_URL_SLUG"]}/raw/names/modular-mac-arm64/versions/0.0.1/modular-0.0.1-macos-arm64.tar.gz"
     sha256 "#{ENV["HOMEBREW_MODULAR_SHA256"]}"
